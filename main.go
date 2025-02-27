c6009eca22e43a048b97cf3c088340b172257f8a
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-02-27 18:23:41 -0500

    Se añade metodo post para guardar en memoria, y se modifica el get para obtener esa informacion en memoria

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"restapi.com/m/models"
 )
 
 func main() {
@@ -11,12 +12,32 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 
 }
 
 func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
 
-	context.JSON(http.StatusOK, gin.H{"message": "!Hello"})
+func createEvent(context *gin.Context) {
+	var event models.Event
+	//bindea la entradaal tipo de objeto de la estructura
+	err := context.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "No se pudo convertir la data del body", "error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Evento creado!", "event": event})
+
 }
